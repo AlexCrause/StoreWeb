bc8fba3f2c842d925f06b379822ea9e356df100e
AlexeiHozyashev <crause937@gmail.com>
2024-12-30 23:36:45 +0500

    added the project structure

---
+++ AuthService/src/main/java/com/example/authservice/controller/AuthController.java
@@ -0,0 +1,32 @@
+package com.example.authservice.controller;
+
+import com.example.authservice.dto.AuthResponse;
+import com.example.authservice.dto.LoginRequest;
+import com.example.authservice.dto.RegisterRequest;
+import com.example.authservice.service.AuthService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/auth")
+@RequiredArgsConstructor
+public class AuthController {
+
+    private final AuthService authService;
+
+//    public AuthController(AuthService authService) {
+//        this.authService = authService;
+//    }
+
+    @PostMapping("/login")
+    public ResponseEntity<AuthResponse> login(@RequestBody LoginRequest request) {
+        return ResponseEntity.ok(authService.login(request));
+    }
+
+    @PostMapping("/register")
+    public ResponseEntity<String> register(@RequestBody RegisterRequest request) {
+        authService.register(request);
+        return ResponseEntity.ok("User registered successfully!");
+    }
+}
---
+++ AuthService/src/main/java/com/example/authservice/service/AuthService.java
@@ -0,0 +1,53 @@
+package com.example.authservice.service;
+
+import com.example.authservice.dto.AuthResponse;
+import com.example.authservice.dto.LoginRequest;
+import com.example.authservice.dto.RegisterRequest;
+import com.example.authservice.model.Role;
+import com.example.authservice.model.User;
+import com.example.authservice.repository.RoleRepository;
+import com.example.authservice.repository.UserRepository;
+import lombok.AllArgsConstructor;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+@AllArgsConstructor
+public class AuthService {
+
+    private final UserRepository userRepository;
+    private final BCryptPasswordEncoder passwordEncoder;
+    private final RoleRepository roleRepository;
+
+//    public AuthService(UserRepository userRepository, BCryptPasswordEncoder passwordEncoder) {
+//        this.userRepository = userRepository;
+//        this.passwordEncoder = passwordEncoder;
+//    }
+
+    public AuthResponse login(LoginRequest request) {
+        User user = userRepository.findByUsername(request.getUsername())
+                .orElseThrow(() -> new RuntimeException("User not found"));
+        if (!passwordEncoder.matches(request.getPassword(), user.getPassword())) {
+            throw new RuntimeException("Invalid credentials");
+        }
+        // Заглушка для токена
+        return new AuthResponse("fake-jwt-token");
+    }
+
+    public void register(RegisterRequest request) {
+        if (userRepository.findByUsername(request.getUsername()).isPresent()) {
+            throw new RuntimeException("User already exists");
+        }
+
+        Role userRole = roleRepository.findByName("USER")
+                .orElseThrow(() -> new RuntimeException("Role not found"));
+
+        User user = new User();
+        user.setUsername(request.getUsername());
+        user.setPassword(passwordEncoder.encode(request.getPassword()));
+        user.getRoles().add(userRole);
+
+        userRepository.save(user);
+    }
+
+}
