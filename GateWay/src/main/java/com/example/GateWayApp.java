f40be28505a7ebf6c0e7d0581f7b2cdc526236c7
AlexeiHozyashev <crause937@gmail.com>
2024-12-30 21:09:37 +0500

    bound authservice with gateway

---
+++ GateWay/src/main/java/com/example/GateWayApp.java
@@ -0,0 +1,21 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class GateWayApp {
+    public static void main(String[] args) {
+        SpringApplication.run(GateWayApp.class, args);
+    }
+
+    @Bean
+    public RouteLocator customRouteLocator(RouteLocatorBuilder builder) {
+        return builder.routes()
+                .route("AuthService",r->r.path("/serviceAuth/**")
+                        .uri("http://localhost:8081/")).build();
+    }
+}
